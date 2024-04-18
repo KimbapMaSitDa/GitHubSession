27cfd8fb6ffdb3ddf3289b6854ed3c6b04756ee3
Huijeong Bae <dbzhtkd@gmail.com>
2024-04-18 20:40:23 +0900

    Second Commit

--- GitHubSession/ContentView.swift
+++ GitHubSession/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, Hannah!")
+            Text("Hello, Hannah!")
+            Text("Hello, Hannah!")
+            Text("Hello, Hannah!")
+            Text("Hello, Hannah!")
         }
         .padding()
     }
