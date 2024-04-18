ee15cba675d32ba97c4cdc302f2848507cf18790
Huijeong Bae <dbzhtkd@gmail.com>
2024-04-18 19:59:58 +0900

    Initial Commit

---
+++ GitHubSession/GitHubSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSessionApp.swift
+//  GitHubSession
+//
+//  Created by Huijeong Bae on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
